7fde0899ca8b3774267e31438110b6de8db31c0e
viktorlv30 <viktor.lv.30@gmail.com>
2016-06-28 21:04:53 +0300

    end lesson 28.06.16

--- dom1.js
+++ dom1.js
@@ -1,3 +1,14 @@
 //location.assign('https://www.google.com.ua/');
 var url = location.toString()
 console.log(url);
+
+console.log(document, document.getElementById('get-id'));
+
+console.log(document.getElementsByName('get-name'));
+console.log(document.getElementsByName('get-name')[0]);
+
+console.log(document.getElementsByTagName('ul'));
+
+console.log(document.getElementsByClassName('get-class'));
+
+console.log(document.getElementById('get-attribute').getAttribute('class'));
